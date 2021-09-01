99a3516825be7bce1a51aa49c707bb5f618a3e14
Ant <huertinez.family@outlook.com>
2021-09-01 12:24:27 -0700

    display repo on page

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,7 +2,7 @@
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 
-var repoContainerEl = document.querySelector("#repos-contianer");
+var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("repo-search-term");
 
 var getUserRepos = function(user) {
@@ -38,25 +38,40 @@ var formSubmitHandler = function(event) {
 var displayRepos = function(repos, searchTerm) {
   repoContainerEl.textContent = "";
   repoSearchTerm.textContent = searchTerm;
-  for (var i = 0; i <repos.length; i++) {
+  for (var i = 0; i < repos.length; i++) {
     // format repo name 
     var repoName = repos[i].owner.login + "/" + repos[i].name;
-
+    
     // create a contianer foe each repo
-    var repoEl = document. createElement("div");
+    var repoEl = document.createElement("div");
     repoEl.classList = "list-item flex-row justify-space-between align-center";
-
+    
     // create a span element to hold repository name
-    var title = document.createElement("span");
-    title.textContent = repoName;
-
+    var titleEl = document.createElement("span");
+    titleEl.textContent = repoName;
+    
     // append to contianer
     repoEl.appendChild(titleEl);
-
+    
+    // create a status element
+    var statusEl = document.createElement("span");
+    statusEl.classList = "flex-row align-center";
+    
+    // check if current repo has issues or not
+    if (repos[i].open_issues_count > 0) {
+      statusEl.innerHTML =
+      "<i class='fas fa-times stauts-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+    } else {
+      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+      
+    }
+    
+    repoEl.appendChild(statusEl);
     // append container to the dom
     repoContainerEl.appendChild(repoEl);
-
+  
   }
+
 };
 
 userFormEl.addEventListener("submit", formSubmitHandler);
